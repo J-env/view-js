ba75ff0d74efe5f6c5dcb58519f8a4245458f053
jxf <1637787429@qq.com>
2022-08-28 19:54:04 +0800

    bootstrap

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -9,6 +9,8 @@ const indexJson = require('../package.json')
 const packagesDir = path.resolve(__dirname, '../packages')
 const files = fs.readdirSync(packagesDir)
 
+const __force = args.force
+
 files.forEach((shortName) => {
   const joinPath = (p) => path.join(packagesDir, shortName, p)
 
@@ -20,48 +22,53 @@ files.forEach((shortName) => {
   const pkgPath = joinPath(`package.json`)
   const pkgExists = fs.existsSync(pkgPath)
 
-  if (pkgExists) {
-    const pkg = require(pkgPath)
+  let pkg_json = {}
 
-    if (pkg.private) {
+  if (pkgExists) {
+    pkg_json = require(pkgPath)
+
+    if (pkg_json.private) {
       return
     }
   }
 
   // package.json
-  if (args.force || !pkgExists) {
-    const json = {
-      name,
-      version: indexJson.version,
-      description: name,
-      main: 'index.js',
-      module: `dist/${shortName}.esm-bundler.js`,
-      files: [`index.js`, `dist`],
-      types: `dist/${shortName}.d.ts`,
-      repository: {
-        ...indexJson.repository,
-        directory: `packages/${shortName}`
-      },
-      bugs: indexJson.bugs,
-      keywords: indexJson.keywords,
-      author: indexJson.author,
-      license: indexJson.license,
-      homepage: `https://github.com/J-env/view-js/tree/main/packages/${shortName}#readme`
-    }
+  pkg_json.name = name
+  pkg_json.version = indexJson.version
+  pkg_json.description = pkg_json.description || name
+  pkg_json.main = pkg_json.main || 'index.js'
+  pkg_json.module = `dist/${shortName}.esm-bundler.js`
+  pkg_json.types = `dist/${shortName}.d.ts`
 
-    fs.writeFileSync(pkgPath, JSON.stringify(json, null, 2))
+  const files = new Set(['index.js', 'dist', ...(pkg_json.files || [])])
+  pkg_json.files = [...files]
+  pkg_json.sideEffects = pkg_json.sideEffects || false
+  pkg_json.scripts = pkg_json.scripts || {}
+  pkg_json.dependencies = pkg_json.dependencies || {}
+  pkg_json.devDependencies = pkg_json.devDependencies || {}
+
+  pkg_json.keywords = indexJson.keywords
+  pkg_json.repository = {
+    ...indexJson.repository,
+    directory: `packages/${shortName}`
   }
+  pkg_json.bugs = indexJson.bugs
+  pkg_json.homepage = indexJson.homepage || `https://github.com/J-env/view-js/tree/master/packages/${shortName}#readme`
+  pkg_json.author = indexJson.author
+  pkg_json.license = indexJson.license
+
+  fs.writeFileSync(pkgPath, JSON.stringify(pkg_json, null, 2))
 
   // README.md
   const readmePath = joinPath(`README.md`)
-  if (args.force || !fs.existsSync(readmePath)) {
+  if (__force || !fs.existsSync(readmePath)) {
     fs.writeFileSync(readmePath, `# ${name}`)
   }
 
   // api-extractor.json
   const apiExtractorConfigPath = joinPath(`api-extractor.json`)
 
-  if (args.force || !fs.existsSync(apiExtractorConfigPath)) {
+  if (__force || !fs.existsSync(apiExtractorConfigPath)) {
     const api_json = {
       extends: `../../api-extractor.json`,
       mainEntryPointFilePath: `./dist/packages/<unscopedPackageName>/src/index.d.ts`,
@@ -77,7 +84,7 @@ files.forEach((shortName) => {
   const srcDir = joinPath(`src`)
   const indexPath = joinPath(`src/index.ts`)
 
-  if (args.force || !fs.existsSync(indexPath)) {
+  if (__force || !fs.existsSync(indexPath)) {
     if (!fs.existsSync(srcDir)) {
       fs.mkdirSync(srcDir)
     }
@@ -88,7 +95,7 @@ files.forEach((shortName) => {
   // index.js
   const nodeIndexPath = joinPath('index.js')
 
-  if (args.force || !fs.existsSync(nodeIndexPath)) {
+  if (__force || !fs.existsSync(nodeIndexPath)) {
     fs.writeFileSync(
       nodeIndexPath,
       `
